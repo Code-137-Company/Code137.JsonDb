5a4fa08bf41a1ae0150b6ae8d3193a6fd141417b
Guilherme Anselmi <60523666+GuilhermeAnselmi@users.noreply.github.com>
2024-01-25 19:09:57 -0300

    Code sample update

--- Code137.JsonDb.Sample/Program.cs
+++ Code137.JsonDb.Sample/Program.cs
@@ -7,43 +7,23 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var jsonDb = new JsonDb(new DatabaseOptions("TestLib", path: AppDomain.CurrentDomain.BaseDirectory, password: "1234"));
-        jsonDb.AddEntity<User>();
-        jsonDb.AddEntity<Licence>();
+        var jsonDb = new JsonDb(new DatabaseOptions("MyDatabase", path: AppDomain.CurrentDomain.BaseDirectory, password: "123456"));
 
-        var users = jsonDb.GetAll<User>();
+        jsonDb.AddEntity<User>();
 
         var user = new User()
         {
-            Name = "Guilherme",
+            Name = "Willian",
             Age = 23,
             Email = "teste@gmail.com"
         };
 
-        var licence = new Licence()
-        {
-            Name = "Default",
-            Days = 30
-        };
-
-        jsonDb.Insert(user, out string messageInsert);
-        jsonDb.Insert(licence, out messageInsert);
+        jsonDb.Insert(user, out string outputMessage);
 
         user = jsonDb.GetById<User>(user.Id);
 
+        jsonDb.Update<User>(user.Id, x => x.Age = 24, out outputMessage);
+
         jsonDb.Delete<User>(user.Id, out string messageDelete);
-
-        jsonDb.Insert(user, out messageInsert);
-
-        users = jsonDb.GetAll<User>();
-
-        user.Age = 24;
-        jsonDb.Update(user, out string messageUpdate);
-
-        users = jsonDb.GetAll<User>();
-
-        jsonDb.Update<User>(user.Id, (x => x.Email = "teste@teste.com"), out messageUpdate);
-
-        users = jsonDb.GetAll<User>();
     }
 }
